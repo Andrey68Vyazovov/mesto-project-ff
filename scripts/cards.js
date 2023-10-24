0fda08bdcc1fbe5ddb4fa261abeb685e5fe6fbbe
Andrey Vyazovov <andreyy-vjazovov@rambler.ru>
2023-10-24 07:41:32 +0300

    ver1.5

--- scripts/cards.js
+++ scripts/cards.js
@@ -2,25 +2,31 @@ const initialCards = [
     {
       name: "Тамбов",
       link: "../images/tambov.jpg",
+      alt: "Вид на высокую колокольню в центре Тамбова."
     },
     {
       name: "Санкт-Петербург",
       link: "../images/petersburg.jpg",
+      alt: "Стрелка Васильевского острова. Здание биржи."
     },
     {
       name: "Эльбрус",
       link: "../images/elbrus.jpg",
+      alt: "Вид на Эльбрус с севера."
     },
     {
       name: "Пятигорск",
       link: "../images/5gorsk.jpg",
+      alt: "Вид на Пятигорск с горы Машук."
     },
     {
       name: "Алтай",
       link: "../images/altai.jpg",
+      alt: "Горный Алтай. Альпийские луга."
     },
     {
       name: "Абхазия",
       link: "../images/abhaziya.jpg",
+      alt: "Абхазия. Гагры."
     },
 ];
--- scripts/index.js
+++ scripts/index.js
@@ -7,22 +7,19 @@ function createCard(card, deleteFunction) {
   const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
   cardElement.querySelector(".card__title").textContent = card.name;
   cardElement.querySelector(".card__image").src = card.link;
+  cardElement.querySelector(".card__image").alt = card.alt;
   cardElement
     .querySelector(".card__delete-button")
-    .addEventListener("click", function (event) {
-      return deleteFunction(event);
-    });
+    .addEventListener("click", deleteFunction);
   return cardElement;
 }
 // @todo: Функция удаления карточки
 function deleteCard(event) {
-  const removeElement = event.target.parentElement;
+  const removeElement = event.target.closest(".card");
   removeElement.remove();
 }
 // @todo: Вывести карточки на страницу
 const appendCards = initialCards.map(function (card) {
   return createCard(card, deleteCard);
 });
-appendCards.forEach(function (card) {
-  return placesList.append(card);
-});
+placesList.append(...appendCards);
