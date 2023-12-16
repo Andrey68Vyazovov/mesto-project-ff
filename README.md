7ac04b825628407118784bff26cb6ef2838a4136
Andrey Vyazovov <andreyy-vjazovov@rambler.ru>
2023-12-16 21:28:56 +0300

    ver3.0

---
+++ README.md
@@ -0,0 +1,5 @@
+# Проектная работа Mesto
+https://github.com/Andrey68Vyazovov/mesto-project-ff
+
+https://andrey68vyazovov.github.io/mesto-project-ff/
+
---
+++ src/scripts/cards.js
@@ -0,0 +1,40 @@
+import tambovImage from '../images/tambov.jpg';
+import petersburgImage from '../images/petersburg.jpg';
+import elbrusImage from '../images/elbrus.jpg';
+import gorskImage from '../images/5gorsk.jpg';
+import altaiImage from '../images/altai.jpg';
+import abhaziyaImage from '../images/abhaziya.jpg';
+
+
+export const initialCards = [
+    {
+      name: "Тамбов",
+      link: tambovImage,
+      alt: "Вид на высокую колокольню в центре Тамбова."
+    },
+    {
+      name: "Санкт-Петербург",
+      link: petersburgImage,
+      alt: "Стрелка Васильевского острова. Здание биржи."
+    },
+    {
+      name: "Эльбрус",
+      link: elbrusImage,
+      alt: "Вид на Эльбрус с севера."
+    },
+    {
+      name: "Пятигорск",
+      link: gorskImage,
+      alt: "Вид на Пятигорск с горы Машук."
+    },
+    {
+      name: "Алтай",
+      link: altaiImage,
+      alt: "Горный Алтай. Альпийские луга."
+    },
+    {
+      name: "Абхазия",
+      link: abhaziyaImage,
+      alt: "Абхазия. Гагры."
+    },
+];
